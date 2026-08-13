bae07069662ce016825007c779b64072490e863b
UpendraShah7 <shahthakuriupendra@gmail.com>
2026-08-13 11:06:08 +0545

    more about events

--- Week_2/Day_4/javas3.js
+++ Week_2/Day_4/javas3.js
@@ -53,3 +53,71 @@ form.addEventListener("submit", (event) => {
 
     console.log("Form submitted!");
 });
+
+
+// let btn1 = document.querySelector("#btn1");
+// btn1.onclick=()=>{
+//   console.log("Button was Clickedddd");
+// };
+
+
+// Another Method of Handling events.
+// function handleclick(){
+//     console.log("button was Clicked");
+// }
+
+// btn1.onclick = handleclick;
+
+
+// Event Object 
+// e.target
+// e.type
+// e.clientX
+// e.clientY
+
+
+
+
+// Event Listener  
+// node.addEventListener(event , callback);
+
+let btn1 = document.querySelector("#btn1");
+btn1.addEventListener("click",()=>{
+        console.log("Button  firstl was clicked")
+});
+
+btn1.addEventListener("click",()=>{
+        console.log("This is second event listener of that button first")
+});
+
+const handler3= ()=>{
+        console.log("This is third event listener of that button first")
+}
+btn1.addEventListener("click", handler3);
+
+
+
+btn1.addEventListener("click",()=>{
+        console.log("This is fourth event listener of that button first")
+});
+
+
+// Note ! here callback reference should be same to callback.
+// node.removeEventListener(event , callback);
+btn1.removeEventListener("click",handler3);
+
+
+let modeBtn = document.querySelector("#mode");
+let currmode= "light";
+modeBtn.addEventListener("click",()=>{
+        if (currmode == "light"){
+            currmode = "dark"
+            console.log(currmode);
+            document.querySelector("body").style.backgroundColor ="black";
+        }
+        else{
+            currmode = "light";
+            console.log(currmode);
+            document.querySelector("body").style.backgroundColor = "white"
+        }
+})
